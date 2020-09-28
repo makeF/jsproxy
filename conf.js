8d7ad3a02f3f14c2c40aa6bc9c750a2e9eab32bd
makeF <2427829589@qq.com>
2020-09-28 10:51:56 +0000

    Update conf.js

--- conf.js
+++ conf.js
@@ -25,11 +25,11 @@ jsproxy_config({
       lines: {
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.jsproxy.workers.dev': 1,
+        //'b.jsproxy.workers.dev': 1,
         'jsproxy.makef.workers.dev': 1,
-        'b.007.workers.dev': 1,
-        'b.hehe.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
+        //'b.007.workers.dev': 1,
+        //'b.hehe.workers.dev': 1,
+        //'b.lulu.workers.dev': 1,
       }
     }
   },
